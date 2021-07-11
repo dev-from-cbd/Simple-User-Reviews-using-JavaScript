fb221ffde6bde6cbe1e7eb32dc03390936ddc5db
dev-from-cbd <veryfatwombat@gmail.com>
2021-07-11 10:15:43 +1000

    js

--- app.js
+++ app.js
@@ -45,7 +45,14 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
-  const item = reviews[currentItem];
+  showPerson(currentItem);
+});
+
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
   img.src = item.img;
   author.textContent = item.name;
-});
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
